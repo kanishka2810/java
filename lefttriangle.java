da4ece8366338dabd2eccf488bb61d8f75af77a4
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-27 12:16:51 +0530

    added some pattern question

---
+++ lefttriangle.java
@@ -0,0 +1,19 @@
+class lefttriangle
+{
+    public static void main(String[] args)
+    {
+        for(int k=1;k<=5;k++)
+        {
+            for(int m=5-k;m>=1;m--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=k;j++)
+            {
+                System.out.print("*");
+            }
+        System.out.println();
+        } 
+    
+    }
+}
