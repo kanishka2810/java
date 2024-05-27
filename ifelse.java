9a3247550c9ad761f86c6b3bec487dc56ca819a4
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-27 10:54:30 +0530

    added iselse

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+class ifelse
+{
+    public static void main(String[] args) {
+        int a=10,b=5;
+        if(a<b)
+            System.out.println("a is lesser");
+        else
+            System.err.println("b is lesser");
+    }
+}
