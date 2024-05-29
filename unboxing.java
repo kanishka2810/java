6fd1551bf243643a1794b0af7cf9f38a6223b5f1
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-29 15:37:51 +0530

    added

---
+++ unboxing.java
@@ -0,0 +1,26 @@
+public class unboxing {
+    public static void main(String[] args) {
+        
+        // Integer a=new Integer(3);
+        // int i=a.intValue();
+        // int j=a;
+        // System.out.println(a+" "+i+" "+j);
+
+        // Character c=new Character('c');
+        // int k 
+        byte b=10;  
+        Byte byteobj=b;  
+        byte bytevalue=byteobj;  
+        System.out.println("byte value: "+bytevalue); 
+
+        float f=20.886F;
+        Float floatobj=f;
+        float floatvalue=floatobj;
+        System.out.println("float value: "+floatvalue);    
+
+        double d=67.865;
+        Double doubleobj=d;
+        double doublevalue=doubleobj;
+        System.out.println("double value: "+doublevalue); 
+    }
+}
