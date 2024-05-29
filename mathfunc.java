d37c3198f7413ac289ad23d9d5b509e74d473005
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-29 12:59:21 +0530

    added

---
+++ mathfunc.java
@@ -0,0 +1,15 @@
+public class mathfunc {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int b=7,a=9;
+        System.out.println("Square root of a is:"+Math.sqrt(a));
+        System.out.println("Maximum between a and b is:"+Math.max(a,b));
+        System.out.println("Minimum between a and b is:"+Math.min(a,b));
+        System.out.println("Logaritham of b is:"+Math.log(a));
+        System.out.println("exp of a is:"+Math.exp(a));
+        System.out.println("Sine value of a is: " +Math.sin(a));    
+        System.out.println("Cosine value of a is: " +Math.cos(a));  
+        System.out.println("Tangent value of a is: " +Math.tan(a));  
+    }
+}
+
