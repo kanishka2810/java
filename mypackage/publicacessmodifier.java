c314d811494fdb0204e7afac740a0455ade87a98
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-29 13:21:34 +0530

    added

---
+++ mypackage/publicacessmodifier.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+import mypack.publicacess;
+
+public class publicacessmodifier extends publicacess {
+   public static void main(String[] args) {
+       publicacess obj=new publicacess();
+       obj.msg();
+   } 
+}
