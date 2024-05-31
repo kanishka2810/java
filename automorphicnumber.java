4a415dea4823fa294442d4560e1cce919b43b261
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-05-31 15:32:07 +0530

    updated

--- automorphicnumber.java
+++ automorphicnumber.java
@@ -7,13 +7,25 @@ public class automorphicnumber {
         System.out.println("Enter a number");
         int num=sc.nextInt();
         int square=num*num;
+        int flag=0;
         while(num>0)
         {
             if(num%10!=square%10)
-                System.out.println("Not equal");
+            {
+                System.out.println("Not Automorphic");
+                flag=0;
+                break;
+            }
+            else{
+                flag=1;
+            }
             num=num/10;
             square=square/10;
+            //flag=1;
         }
-        System.out.println("Automorphic");
+        if(flag==1)
+            System.out.println(" Automorphic");
+        //else
+          //  System.out.println("automorphic");
     }
 }
