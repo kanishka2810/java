7781e468b986ceac1f25e724f67912a7a1486027
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-06-11 09:39:44 +0530

    added

---
+++ interchangeelementofrowinmatrix.java
@@ -0,0 +1,32 @@
+public class interchangeelementofrowinmatrix {
+    public static void main(String args[])
+	{
+		int a[][] = { {2,8,1,0},
+						{ 1,1,1,0 },
+						{ 0,6,0,2},
+						{ 0,9,0,3 } };
+		System.out.println("Input matrix is as follows : ");
+		for (int j = 0; j < a.length; j++) 
+		{
+			for (int k = 0; k < a[0].length; k++)
+			    System.out.print(a[j][k] + " ");
+			System.out.println();
+		}	
+		int res = a.length;
+		
+		for (int j = 0; j < a[1].length; j++) 
+		{
+		    
+			int temp = a[1][j];
+			a[1][j] = a[res - 2][j];
+			a[res - 2][j] = temp;
+		}	
+		System.out.println("Output matrix is as follows : ");
+		for (int j = 0; j < a.length; j++) 
+		{
+			for (int k = 0; k < a[0].length; k++)
+				System.out.print(a[j][k] + " ");
+			System.out.println();
+		}
+	}
+}
