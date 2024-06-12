35f4e96ee5a0079b5dd5027dcd86c9e4142d01b8
kanishka <kanishka.khandelwal.9828@gmail.com>
2024-06-12 10:52:05 +0530

    added

---
+++ Minimizethemaxdiffebwtheheight.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class Minimizethemaxdiffebwtheheight 
+{
+    public static void main(String[] args)
+	{
+		int[] a = {1, 15, 10};
+		int k = 6;
+		int n=a.length;
+
+		Arrays.sort(a);
+		int ans = a[n - 1] - a[0];
+
+		int min, max;
+		// min = a[0];
+		//max = a[n - 1];
+
+		for (int i = 1; i < n; i++) 
+        {
+			if (a[i] - k < 0)
+				continue;
+			min = Math.min(a[0] + k, a[i] - k);
+			max = Math.max(a[i - 1] + k, a[n - 1] - k);
+			ans = Math.min(ans, max - min);
+		}
+		System.out.println(ans);
+	}
+}
